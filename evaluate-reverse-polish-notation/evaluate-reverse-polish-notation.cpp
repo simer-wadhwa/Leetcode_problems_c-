44a851e8831071c305acfa47b982edb2a6fc8f01
simer-wadhwa <67890143+simer-wadhwa@users.noreply.github.com>
2021-07-09 11:18:25 +0530

    Time: 28 ms (7.22%), Space: 11.9 MB (54.94%) - LeetHub

--- evaluate-reverse-polish-notation/evaluate-reverse-polish-notation.cpp
+++ evaluate-reverse-polish-notation/evaluate-reverse-polish-notation.cpp
@@ -1,38 +1,34 @@
-#include <stack>
 class Solution {
 public:
     int evalRPN(vector<string>& tokens) {
-        stack<int>s;
-        int n=tokens.size();
-       // int x,y,ans;
-        //s.push(stoi(tokens[0]));
-        //s.push(stoi(tokens[1]));
-        for(int i=0;i<n;i++)
+        stack<int> st;
+        for(int i=0;i<tokens.size();i++)
         {
-            if(tokens[i]=="+"||tokens[i]=="-"||tokens[i]=="/"||tokens[i]=="*")
-            {
-               int x=s.top();
-                s.pop();
-               int y=s.top();
-                s.pop();
-                if(tokens[i]=="+")
-                    s.push(y + x);
-               
-               else if(tokens[i]=="-")
-                    s.push(y - x);
-               
-               else if(tokens[i]=="*")
-                    s.push(y * x);
-                
-               else if(tokens[i]=="/")
-                    s.push(y / x);
-                
-                //s.push(ans);
+          if(tokens[i]=="+"||tokens[i]=="-"||tokens[i]=="*"||tokens[i]=="/")
+          {
+              int v1=st.top();
+              st.pop();
+              int v2=st.top();
+              st.pop();
+              //int r = a tokens[i] b;
+              //stack.push(r);
+              if(tokens[i]=="+"){
+                st.push(v2+v1);  /// push result inside stack
+            } 
+            else if(tokens[i]=="-"){
+                st.push(v2-v1);  /// push result inside stack
             }
+             else if(tokens[i]=="*"){
+                st.push(v2*v1);  /// push result inside stack
+            }
+             else if(tokens[i]=="/"){
+                st.push(v2/v1);  /// push result inside stack
+            }
+              
+          }
             else
-                s.push(atoi(tokens[i].c_str()));
+                st.push(atoi(tokens[i].c_str()));
         }
-        
-        return s.top();
+      return st.top();  
     }
 };
